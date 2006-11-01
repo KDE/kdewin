d83f719aa849d179d9c3114eac2bf285a00187f7
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2006-11-01 20:00:44 +0000

    compile ++
    svn path=/trunk/kdesupport/kdewin32/; revision=601039

--- tests/time.c
+++ tests/time.c
@@ -1,23 +1,25 @@
 
 #include <sys/time.h>
+#include <stdio.h>
+#include <unistd.h>
 #include <windows.h>
 
 int verbose = 0;
 
 int main(int c, char **argv)
 {
-    struct timeval  tv;
+    struct timeval  tv, tt;
+    long long int t1, t2;
     gettimeofday(&tv, NULL);
-    long long int t1 = ((long long int)tv.tv_sec) * 1000000LL + tv.tv_usec;
+    t1 = ((long long int)tv.tv_sec) * 1000000LL + tv.tv_usec;
 		if (verbose) {
 			printf("1 sec:  %d\n",tv.tv_sec); 
 			printf("1 usec: %d\n",tv.tv_usec); 
 			printf("1 sec+usec:  %ld\n",t1); 
 		}
 		sleep(1);
-    struct timeval  tt;
     gettimeofday(&tt, NULL);
-    long long int t2 = ((long long int)tt.tv_sec) * 1000000LL + tt.tv_usec;
+    t2 = ((long long int)tt.tv_sec) * 1000000LL + tt.tv_usec;
 		if (verbose) {
 			printf("2 sec:  %d\n",tt.tv_sec); 
 			printf("2 usec: %d\n",tt.tv_usec); 
