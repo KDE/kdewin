a4facb3fe17e6fc1b9715691430baf1f9e723eb1
Brad Hards <bradh@frogmouth.net>
2007-01-02 23:34:34 +0000

    Fix a few spelling errors, as identified by Krazy.
    svn path=/trunk/kdesupport/kdewin32/; revision=619202

--- include/directx/ddraw.h
+++ include/directx/ddraw.h
@@ -371,9 +371,9 @@ typedef struct _DDCAPS_DX7		/* DirectX 7 version of caps struct */
 {
     DWORD	dwSize;                 /* size of the DDDRIVERCAPS structure */
     DWORD	dwCaps;                 /* driver specific capabilities */
-    DWORD	dwCaps2;                /* more driver specific capabilites */
+    DWORD	dwCaps2;                /* more driver specific capabilities */
     DWORD	dwCKeyCaps;             /* color key capabilities of the surface */
-    DWORD	dwFXCaps;               /* driver specific stretching and effects capabilites */
+    DWORD	dwFXCaps;               /* driver specific stretching and effects capabilities */
     DWORD	dwFXAlphaCaps;          /* alpha driver specific capabilities */
     DWORD	dwPalCaps;              /* palette capabilities */
     DWORD	dwSVCaps;               /* stereo vision capabilities */
@@ -432,9 +432,9 @@ typedef struct _DDCAPS_DX6		/* DirectX 6 version of caps struct */
 {
     DWORD	dwSize;                 /* size of the DDDRIVERCAPS structure */
     DWORD	dwCaps;                 /* driver specific capabilities */
-    DWORD	dwCaps2;                /* more driver specific capabilites */
+    DWORD	dwCaps2;                /* more driver specific capabilities */
     DWORD	dwCKeyCaps;             /* color key capabilities of the surface */
-    DWORD	dwFXCaps;               /* driver specific stretching and effects capabilites */
+    DWORD	dwFXCaps;               /* driver specific stretching and effects capabilities */
     DWORD	dwFXAlphaCaps;          /* alpha driver specific capabilities */
     DWORD	dwPalCaps;              /* palette capabilities */
     DWORD	dwSVCaps;               /* stereo vision capabilities */
@@ -494,9 +494,9 @@ typedef struct _DDCAPS_DX5		/* DirectX5 version of caps struct */
 {
     DWORD	dwSize;                 /* size of the DDDRIVERCAPS structure */
     DWORD	dwCaps;                 /* driver specific capabilities */
-    DWORD	dwCaps2;                /* more driver specific capabilites */
+    DWORD	dwCaps2;                /* more driver specific capabilities */
     DWORD	dwCKeyCaps;             /* color key capabilities of the surface */
-    DWORD	dwFXCaps;               /* driver specific stretching and effects capabilites */
+    DWORD	dwFXCaps;               /* driver specific stretching and effects capabilities */
     DWORD	dwFXAlphaCaps;          /* alpha driver specific capabilities */
     DWORD	dwPalCaps;              /* palette capabilities */
     DWORD	dwSVCaps;               /* stereo vision capabilities */
@@ -555,9 +555,9 @@ typedef struct _DDCAPS_DX3		/* DirectX3 version of caps struct */
 {
     DWORD	dwSize;                 /* size of the DDDRIVERCAPS structure */
     DWORD	dwCaps;                 /* driver specific capabilities */
-    DWORD	dwCaps2;                /* more driver specific capabilites */
+    DWORD	dwCaps2;                /* more driver specific capabilities */
     DWORD	dwCKeyCaps;             /* color key capabilities of the surface */
-    DWORD	dwFXCaps;               /* driver specific stretching and effects capabilites */
+    DWORD	dwFXCaps;               /* driver specific stretching and effects capabilities */
     DWORD	dwFXAlphaCaps;          /* alpha driver specific capabilities */
     DWORD	dwPalCaps;              /* palette capabilities */
     DWORD	dwSVCaps;               /* stereo vision capabilities */
