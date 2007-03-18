7fb855055375df735855a482fe74b0f9c982849f
Holger Schröder <holger@holgis.net>
2007-03-18 23:17:31 +0000

    add openssl package
    svn path=/trunk/kdesupport/kdewin32/; revision=643991

---
+++ emerge/portage/gnuwin32/openssl/openssl-0.9.7c.py
@@ -0,0 +1,24 @@
+import gnuwin32
+import os
+
+SRC_URI = """
+http://switch.dl.sf.net/sourceforge/gnuwin32/openssl-0.9.7c-bin.zip
+http://switch.dl.sf.net/sourceforge/gnuwin32/openssl-0.9.7c-lib.zip
+"""
+
+class subclass(gnuwin32.gnuwin32class):
+  def __init__(self):
+    gnuwin32.gnuwin32class.__init__( self, SRC_URI )
+    
+  def install( self ):
+    print "openssl install called"
+    
+    # remove this file, because it collides with the one from wget
+    os.remove( os.path.join( self.workdir, "bin", "libeay32.dll" ) )
+    
+    gnuwin32.gnuwin32class.install( self )
+    
+    return True
+
+subclass().execute()
+
