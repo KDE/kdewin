bbc31bc970dcb71c27f47456fce9e2519dacdfdd
Holger Schröder <holger@holgis.net>
2007-03-19 17:01:18 +0000

    some work on qt for mingw
    svn path=/trunk/kdesupport/kdewin32/; revision=644291

--- emerge/portage/libs/qt/qt-4.2.2.py
+++ emerge/portage/libs/qt/qt-4.2.2.py
@@ -26,21 +26,29 @@ class subclass(base.baseclass):
   def unpack( self ):
     # apply patch from windbus to enable qdbus
     print "qt unpack called"
-    utils.unpackFiles( self.downloaddir, self.filenames, self.workdir )
+    utils.unpackFiles( self.downloaddir, ["qt-win-opensource-src-4.2.2.zip"], \
+	self.workdir )
+	
+    utils.unpackFile( self.downloaddir, "acs-4.2.2-patch1.zip", \
+	os.path.join( self.workdir, "qt-win-opensource-src-4.2.2" ) )
+
     os.chdir( os.path.join( self.workdir, "qt-win-opensource-src-4.2.2" ) )
-    #patchfile = os.path.join( self.rootdir, \
-    #    "distfiles\\svn-src\\windbus\\trunk\\QtDBus-win32.patch" )
-    #command = "patch -p0 < %s" % patchfile
-    #os.system( command ) and die( "qt patch" )
+    patchfile = os.path.join( self.rootdir, \
+        "distfiles\\svn-src\\dbus\\trunk\\QtDBus-win32.patch" )
+    command = "patch -p0 < %s" % patchfile
+    os.system( command ) and die( "qt patch" )
     
     # disable demo and example subdirs in main .pro file
-    #file = "projects.pro"		
-    #os.rename( file, "%s.orig" % file )
-    #sedcommand = """ "s/\(SUBDIRS += demos\)/#\\1/" """ 		
-    #command = "type %s.orig | sed -e %s > %s" % ( file, sedcommand, file )
-    #print "command:", command
-    #os.system( command )and die( "qt sed" )
+    file = "projects.pro"		
+    os.rename( file, "%s.orig" % file )
+    sedcommand = """ "s/\(SUBDIRS += demos\)/#\\1/" """ 		
+    command = "type %s.orig | sed -e %s > %s" % ( file, sedcommand, file )
+    print "command:", command
+    os.system( command ) and die( "qt sed" )
 
+    # apply additional compiler support patch
+    os.system( r"qtwin_patch\patch.exe -p1 -i qtwin_patch\msvc_bcc32_42.patch") \
+	and die ( "installpatch" )
     return True
     
   def compile( self ):
@@ -73,10 +81,10 @@ class subclass(base.baseclass):
     print "qt install called"
     qtsrcdir = os.path.join( self.workdir, "qt-win-opensource-src-4.2.2" )
     os.chdir( qtsrcdir )
-    ##os.system( "mingw32-make INSTALL_ROOT=%s install" % self.imagedir[2:] )
+    os.system( "mingw32-make INSTALL_ROOT=%s install" % self.imagedir[2:] )
     os.chdir( "tools\\qdbus" )
-    ##os.system( "mingw32-make INSTALL_ROOT=%s install" % self.imagedir[2:] )
-    ##utils.moveImageDirContents( self.imagedir, qtsrcdir[3:], "qt" )
+    os.system( "mingw32-make INSTALL_ROOT=%s install" % self.imagedir[2:] )
+    utils.moveImageDirContents( self.imagedir, qtsrcdir[3:], "qt" )
 
     # hmm, some programs are not installed, for example qmake.exe ...
     srcbindir = os.path.join( qtsrcdir, "bin" )
