846778a458a1fd281b8a9a370f3be4cc18d3c5c8
Holger Schröder <holger@holgis.net>
2007-03-20 15:34:37 +0000

    add DOWNLOADDIR env var
    svn path=/trunk/kdesupport/kdewin32/; revision=644618

--- emerge/kdesettings-example.bat
+++ emerge/kdesettings-example.bat
@@ -16,8 +16,25 @@ set PYTHONPATH=e:\python25
 
 set PATH=%PATH%;%PYTHONPATH%
 
+: here you can set the download directory to another dir
+: if you want, so you can share the same download dir between
+: mingw and msvc
+set DOWNLOADDIR=e:\distfiles
+
+: here you can tell emerge in which dir you want to save the
+: svn checkout of kde sources, and you can optionally
+: set the svn.kde.org server instead of the anonsvn.kde.org
+: one, so that you can directly commit your changes from the
+: svn checkout from emerge, if you want.
+set KDESVNDIR=e:\kdesvn
+set KDESVNSERVER=svn://anonsvn.kde.org
+set KDESVNUSERNAME=username
+set KDESVNPASSWORD=password
+
 @echo kdesettings.bat executed
 @echo KDEROOT     : %KDEROOT%
 @echo KDECOMPILER : %KDECOMPILER%
+@echo KDESVNDIR   : %KDESVNDIR%
 @echo PYTHONPATH  : %PYTHONPATH%
+@echo DOWNLOADDIR : %DOWNLOADDIR%
 
