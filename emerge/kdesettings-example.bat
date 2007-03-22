d89669e79f69ab4cff66bc44b0ed7fa7ca05718b
Holger Schröder <holger@holgis.net>
2007-03-22 15:06:45 +0000

    : -> rem
    svn path=/trunk/kdesupport/kdewin32/; revision=645411

--- emerge/kdesettings-example.bat
+++ emerge/kdesettings-example.bat
@@ -1,34 +1,38 @@
-: here you set the base directory onder which the whole kde
-: system will live
+rem here you set the base directory onder which the whole kde
+rem system will live
 set KDEROOT=e:\mingwroot
 
-: here you set the compiler to be used:
-: mingw : use the mingw gcc compiler (recommended)
-: msvc2005: use the microsoft visual studio 2005 c-compiler
+rem here you set the compiler to be used:
+rem mingw : use the mingw gcc compiler (recommended)
+rem msvc2005: use the microsoft visual studio 2005 c-compiler
 set KDECOMPILER=mingw
 
-: here you should set the path to your python installation
-: this is needed, so that python will be found, when
-: python scripts will be executed. by setting this here,
-: you don´t have to change the environment settings of
-: windows at all
+rem here you should set the path to your python installation
+rem this is needed, so that python will be found, when
+rem python scripts will be executed. by setting this here,
+rem you don´t have to change the environment settings of
+rem windows at all
 set PYTHONPATH=e:\python25
 
 set PATH=%PATH%;%PYTHONPATH%
 
-: here you can set the download directory to another dir
-: if you want, so you can share the same download dir between
-: mingw and msvc
+rem here you can set the download directory to another dir
+rem if you want, so you can share the same download dir between
+rem mingw and msvc
 set DOWNLOADDIR=e:\distfiles
 
-: here you can tell emerge in which dir you want to save the
-: svn checkout of kde sources, and you can optionally
-: set the svn.kde.org server instead of the anonsvn.kde.org
-: one, so that you can directly commit your changes from the
-: svn checkout from emerge, if you want.
+rem here you can tell emerge in which dir you want to save the
+rem svn checkout of kde sources, and you can optionally
+rem set the svn.kde.org server instead of the anonsvn.kde.org
+rem one, so that you can directly commit your changes from the
+rem svn checkout from emerge, if you want.
 set KDESVNDIR=e:\kdesvn
 set KDESVNSERVER=svn://anonsvn.kde.org
 set KDESVNUSERNAME=username
+
+rem for security reasons you better log in to the kde server by hand
+rem and set the username and password at first login. then svn will
+rem remember the password and it does not have to be written down here
 set KDESVNPASSWORD=password
 
 @echo kdesettings.bat executed
