931838be96ced972e9a901471c1ecec07197b344
Holger Schröder <holger@holgis.net>
2007-06-06 20:25:39 +0000

    add new version, remove old one
    svn path=/trunk/kdesupport/kdewin32/; revision=672350

--- emerge/portage/virtual/base/base-0.2.py
+++ emerge/portage/virtual/base/base-0.2.py
@@ -2,12 +2,10 @@ import base
 
 DEPENDS = """
 dev-util/cmake
-dev-util/mc
 dev-util/mingw
 dev-util/perl
 dev-util/subversion
 dev-util/win32libs
-gnuwin32/grep
 gnuwin32/patch
 gnuwin32/sed
 gnuwin32/wget
