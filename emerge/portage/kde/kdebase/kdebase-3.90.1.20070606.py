6b57418298525217635a1dc2f89958cdda2e7119
Holger Schröder <holger@holgis.net>
2007-06-06 21:32:31 +0000

    kdebase update
    svn path=/trunk/kdesupport/kdewin32/; revision=672379

---
+++ emerge/portage/kde/kdebase/kdebase-3.90.1.20070606.py
@@ -0,0 +1,21 @@
+import base
+
+DEPEND = """
+kde/kdelibs
+kde/kdepimlibs
+"""
+
+class subclass(base.baseclass):
+  def __init__(self):
+    base.baseclass.__init__( self, "" )
+
+  def unpack( self ):
+    return self.kdeSvnUnpack( "trunk/KDE", "kdebase" )
+
+  def compile( self ):
+    return self.kdeCompile()
+
+  def install( self ):
+    return self.kdeInstall()
+		
+subclass().execute()
