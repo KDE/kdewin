e7f8102a66ec557b1b1f2b650b2b4a7f26cacf35
Holger Schröder <holger@holgis.net>
2007-06-06 21:27:05 +0000

    kdegames update. i can play kmahjongg and kmines :-)
    svn path=/trunk/kdesupport/kdewin32/; revision=672376

---
+++ emerge/portage/kde/kdegames/kdegames-3.90.1.20070605.py
@@ -0,0 +1,33 @@
+import base
+import utils
+import os
+
+DEPEND = """
+kde/kdelibs
+"""
+
+class subclass(base.baseclass):
+  def __init__(self):
+    base.baseclass.__init__( self, "" )
+
+  def unpack( self ):
+    self.kdeSvnUnpack( "trunk/KDE/", "kdegames" )
+    
+    # for now disable libksirtet and ksirtet, they do not compile under mingw
+    
+    sed = r"""-e "s/add_subdirectory(libksirtet)/#add_subdirectory(libksirtet)/" """ \
+          r"""-e "s/macro_optional_add_subdirectory(ksirtet)/#no ksirtet/" """ \
+          r"""-e "s/macro_optional_add_subdirectory(kblackbox)/#no kblackbox/" """
+	  
+    utils.sedFile( os.path.join( self.workdir, self.package ),
+	"CMakeLists.txt", sed )
+    
+    return True
+
+  def compile( self ):
+    return self.kdeCompile()
+    
+  def install( self ):
+    return self.kdeInstall()
+
+subclass().execute()
