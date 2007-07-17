3c9634dbfde48bec731f7c60b1202f52ba4fba3f
Frank Osterfeld <frank.osterfeld@kdemail.net>
2007-07-17 12:51:29 +0000

    add mingw/bin and move everything not edited by the user to the end
    svn path=/trunk/kdesupport/kdewin32/; revision=689018

--- emerge/kdesettings-example.bat
+++ emerge/kdesettings-example.bat
@@ -2,17 +2,6 @@ rem here you set the base directory under which the whole kde
 rem system will live
 set KDEROOT=e:\mingwroot
 
-set PATH=%PATH%;%KDEROOT%\gnuwin32\bin
-set PATH=%PATH%;%KDEROOT%\qt\bin
-set PATH=%PATH%;%KDEROOT%\qt\lib
-set PATH=%PATH%;%KDEROOT%\cmake\bin
-set PATH=%PATH%;%KDEROOT%\win32libs\bin
-set PATH=%PATH%;%KDEROOT%\kde\bin
-set PATH=%PATH%;%KDEROOT%\kde\lib
-set PATH=%PATH%;%KDEROOT%\dbus\bin
-set KDEWIN_DIR=%KDEROOT%\win32libs
-set STRIGI_HOME=%KDEROOT%\kde
-
 rem here you set the compiler to be used:
 rem mingw : use the mingw gcc compiler (recommended)
 rem msvc2005: use the microsoft visual studio 2005 c-compiler
@@ -24,8 +13,6 @@ rem python scripts will be executed. by setting this here,
 rem you don´t have to change the environment settings of
 rem windows at all
 set PYTHONPATH=e:\python25
-
-set PATH=%PATH%;%PYTHONPATH%
 
 rem here you can set the download directory to another dir
 rem if you want, so you can share the same download dir between
@@ -45,12 +32,28 @@ rem and set the username and password at first login. then svn will
 rem remember the password and it does not have to be written down here
 rem set KDESVNPASSWORD=password
 
-
 rem if you use svn+ssh, you'll need a ssh-agent equaivalent for managing the authorization 
 rem Pageant is provided by putty, get it at 
 rem http://www.chiark.greenend.org.uk/~sgtatham/putty/download.html
 rem and make sure plink is in your path and Pageant is configured (you need to import your key)
 set SVN_SSH=plink 
+
+rem No editing should be necessary below this line (in an ideal world)
+rem ##################################################################
+
+set PATH=%PATH%;%PYTHONPATH%
+set PATH=%PATH%;%KDEROOT%\mingw\bin
+set PATH=%PATH%;%KDEROOT%\gnuwin32\bin
+set PATH=%PATH%;%KDEROOT%\qt\bin
+set PATH=%PATH%;%KDEROOT%\qt\lib
+set PATH=%PATH%;%KDEROOT%\cmake\bin
+set PATH=%PATH%;%KDEROOT%\win32libs\bin
+set PATH=%PATH%;%KDEROOT%\kde\bin
+set PATH=%PATH%;%KDEROOT%\kde\lib
+set PATH=%PATH%;%KDEROOT%\dbus\bin
+set KDEWIN_DIR=%KDEROOT%\win32libs
+set STRIGI_HOME=%KDEROOT%\kde
+
 
 @echo kdesettings.bat executed
 @echo KDEROOT     : %KDEROOT%
