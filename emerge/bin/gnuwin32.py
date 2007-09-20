b9867b8b4ea2c1864e268ab5d3627d6e1413ee83
Patrick Spendrin <ps_ml@gmx.de>
2007-09-20 01:08:21 +0000

    quieting this one as well
    svn path=/trunk/kdesupport/kdewin32/; revision=714606

--- emerge/bin/gnuwin32.py
+++ emerge/bin/gnuwin32.py
@@ -7,7 +7,8 @@ class gnuwin32class(base.baseclass):
     base.baseclass.__init__( self, SRC_URI )
 
   def install( self ):
-    print "gnuwin32 install called"
+    if not self.stayQuiet:
+        print "gnuwin32 install called"
     destdir = os.path.join( self.imagedir, "gnuwin32" )
     utils.copySrcDirToDestDir( self.workdir, destdir )
     return True
