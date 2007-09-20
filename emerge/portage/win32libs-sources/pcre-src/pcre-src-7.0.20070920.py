8c33dc1f894d2b22cb05de8e118405e9c74573aa
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2007-09-20 16:15:13 +0000

    fix pcre-src to coompile with msvc
    svn path=/trunk/kdesupport/kdewin32/; revision=714788

--- emerge/portage/win32libs-sources/pcre-src/pcre-src-7.0.20070920.py
+++ emerge/portage/win32libs-sources/pcre-src/pcre-src-7.0.20070920.py
@@ -6,7 +6,7 @@ from utils import die
 
 PACKAGE_NAME         = "pcre"
 PACKAGE_VER          = "7.0"
-PACKAGE_FULL_VER     = "7.0"
+PACKAGE_FULL_VER     = "7.0-2"
 PACKAGE_FULL_NAME    = "%s-%s" % ( PACKAGE_NAME, PACKAGE_VER )
 PACKAGE_DLL_NAME     = "pcre"
 
@@ -24,21 +24,7 @@ class subclass(base.baseclass):
     base.baseclass.__init__( self, SRC_URI )
     self.createCombinedPackage = False
     self.instsrcdir = PACKAGE_FULL_NAME
-
-  def kdeDefaultDefines( self ):
-    # adjust some vars for proper compile
-    cmake_src  = os.path.join( self.workdir, self.instsrcdir )
-    cmake_dest = os.path.join( self.imagedir, self.instdestdir )
-
-    options = "%s -DCMAKE_INSTALL_PREFIX=%s " % \
-              ( cmake_src, cmake_dest.replace( '\\', '/' ) )
-    #options = options + "-DCMAKE_INCLUDE_PATH=%s " % \
-    #          os.path.join( self.rootdir, "win32libs", "include" ).replace( "\\", "/" )
-
-    #options = options + "-DCMAKE_LIBRARY_PATH=%s " % \
-    #          os.path.join( self.rootdir, "win32libs", "lib" ).replace( "\\", "/" )
-
-    return options
+    self.buildType = "Release"
 
   def compile( self ):
 
@@ -62,9 +48,6 @@ pcre.h.cmake
     return self.kdeInstall()
 
   def make_package( self ):
-    # auto-create both import libs with the help of pexports
-    self.stripLibs( PACKAGE_DLL_NAME )
-
     # now do packaging with kdewin-packager
     self.doPackaging( PACKAGE_NAME, PACKAGE_FULL_VER, True )
 
