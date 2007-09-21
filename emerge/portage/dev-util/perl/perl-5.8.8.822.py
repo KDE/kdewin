d54289c575860eb53becb648cb1b687d22d5da17
Patrick Spendrin <ps_ml@gmx.de>
2007-09-21 23:22:22 +0000

    installer-layout++ + fixing perl install
    svn path=/trunk/kdesupport/kdewin32/; revision=715369

--- emerge/portage/dev-util/perl/perl-5.8.8.822.py
+++ emerge/portage/dev-util/perl/perl-5.8.8.822.py
@@ -5,7 +5,8 @@ SRC_URI= "http://downloads.activestate.com/ActivePerl/Windows/5.8/ActivePerl-5.8
 class subclass(base.baseclass):
   def __init__(self):
     base.baseclass.__init__( self, SRC_URI )
-    self.instsrcdir = "ActivePerl-5.8.8.820-MSWin32-x86-274739\\perl"
-    self.instdestdir = "perl"
+    self.instsrcdir = "ActivePerl-5.8.8.822-MSWin32-x86-280952\\perl"
+    if self.traditional:
+        self.instdestdir = "perl"
 		
 subclass().execute()
