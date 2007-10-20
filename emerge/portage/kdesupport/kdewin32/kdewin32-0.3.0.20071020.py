843f702f6d5994b90d199dc75230df183a3cb70a
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2007-10-20 14:56:27 +0000

    update to 0.3.0
    svn path=/trunk/kdesupport/kdewin32/; revision=727441

---
+++ emerge/portage/kdesupport/kdewin32/kdewin32-0.3.0.20071020.py
@@ -0,0 +1,28 @@
+import base
+import utils
+from utils import die
+import os
+
+DEPEND = """
+virtual/base
+"""
+
+class subclass(base.baseclass):
+  def __init__(self):
+    base.baseclass.__init__( self, "" )
+
+  def unpack( self ):
+    return self.kdeSvnUnpack( "trunk/kdesupport", "kdewin32" )
+
+  def compile( self ):
+    return self.kdeCompile()
+
+  def install( self ):
+    return self.kdeInstall()
+
+  def make_package( self ):
+    self.instdestdir = "kde"
+    self.instsrcdir = "kdewin32"
+    return self.doPackaging( "kdewin32", "0.3.0-1", True )
+
+subclass().execute()
