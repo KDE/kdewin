6a03b372e73367950599ff0f168ee11989cc629c
Patrick Spendrin <ps_ml@gmx.de>
2007-10-23 17:03:59 +0000

    adding cfitsio library for kdeedu/kstars
    svn path=/trunk/kdesupport/kdewin32/; revision=728543

---
+++ emerge/portage/win32libs-sources/cfitsio-src/cfitsio-src-3.06.py
@@ -0,0 +1,73 @@
+import base
+import os
+import shutil
+import re
+import utils
+from utils import die
+
+PACKAGE_NAME         = "cfitsio"
+PACKAGE_VER          = "3.06"
+PACKAGE_FULL_VER     = "3060"
+PACKAGE_FULL_NAME    = "%s-%s" % ( PACKAGE_NAME, PACKAGE_FULL_VER )
+
+SRC_URI= """
+ftp://heasarc.gsfc.nasa.gov/software/fitsio/c/""" + PACKAGE_NAME + PACKAGE_FULL_VER + """.tar.gz
+"""
+
+DEPEND = """
+"""
+
+#
+# this library is used by kdeedu/kstars
+# a build with msvc should be possible, I just don't have enough time
+#
+
+class subclass(base.baseclass):
+    def __init__(self):
+        base.baseclass.__init__( self, SRC_URI )
+        self.instsrcdir = PACKAGE_NAME
+        self.createCombinedPackage = True
+
+    def execute( self ):
+        base.baseclass.execute( self )
+        if self.compiler <> "mingw":
+            print "error: can only be build with MinGW right now."
+            exit( 1 )
+
+    def unpack( self ):
+        if( not base.baseclass.unpack( self ) ):
+            return False
+            
+        src = os.path.join( self.workdir, self.instsrcdir )
+
+        cmd = "cd %s && patch -p0 < %s" % \
+              ( os.path.join( self.workdir, self.instsrcdir ), os.path.join( self.packagedir , "configure.diff" ) )
+        if not self.stayQuiet:
+            print cmd
+        os.system( cmd ) or die
+
+        cmd = "cd %s && patch -p0 < %s" % \
+              ( os.path.join( self.workdir, self.instsrcdir ), os.path.join( self.packagedir , "Makefile.in.diff" ) )
+        if not self.stayQuiet:
+            print cmd
+        os.system( cmd ) or die
+        
+        return True
+
+    def msysConfigureFlags ( self ):
+        flags = "--prefix=/ "
+        return flags
+
+    def compile( self ):
+        return self.msysCompile( False )
+
+    def install( self ):
+        return self.msysInstall( False )
+
+    def make_package( self ):
+        # now do packaging with kdewin-packager
+        self.doPackaging( PACKAGE_NAME, PACKAGE_FULL_VER, True )
+
+        return True
+
+subclass().execute()
