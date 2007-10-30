96f0f469d2e7083b39be7ddd39eb46da06c7a686
Patrick Spendrin <ps_ml@gmx.de>
2007-10-30 20:43:28 +0000

    added file - last commit just deleted the old one
    svn path=/trunk/kdesupport/kdewin32/; revision=731143

---
+++ emerge/portage/kde/kdeedu/kdeedu-3.94.1.20071030.py
@@ -0,0 +1,29 @@
+import base
+import utils
+import os
+
+DEPEND = """
+kde/kdebase
+"""
+OPTIONAL_DEPEND = """
+kdesupport/eigen
+win32libs-sources/cfitsio-src
+win32libs-sources/openbabel-src
+"""
+class subclass(base.baseclass):
+    def __init__(self):
+        base.baseclass.__init__( self, "" )
+
+    def unpack( self ):
+        return self.kdeSvnUnpack( "trunk/KDE", "kdeedu" )
+
+    def compile( self ):
+        return self.kdeCompile()
+
+    def install( self ):
+        return self.kdeInstall()
+
+    def make_package( self ):
+        return self.doPackaging( "kdeedu", "3.94-1", True )
+
+subclass().execute()
