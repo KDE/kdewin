8f8e4861644cfaa704038f7da2821757c84e437d
Patrick Spendrin <ps_ml@gmx.de>
2007-11-03 00:20:35 +0000

    adding a new build tool required for boost.python / boost.library etc.
    svn path=/trunk/kdesupport/kdewin32/; revision=732177

---
+++ emerge/portage/dev-util/bjam/bjam-3.1.15.20071103.py
@@ -0,0 +1,26 @@
+﻿import base
+import os
+import shutil
+
+PACKAGE_NAME         = "boost-jam"
+PACKAGE_VER          = "3.1.15"
+PACKAGE_FULL_VER     = "3.1.15-1"
+PACKAGE_FULL_NAME    = "%s-%s" % ( PACKAGE_NAME, PACKAGE_FULL_VER)
+
+SRC_URI= """
+http://downloads.sourceforge.net/boost/""" + PACKAGE_FULL_NAME + """-ntx86.zip
+"""
+
+DEPEND = """
+"""
+
+class subclass(base.baseclass):
+    def __init__(self):
+        base.baseclass.__init__( self, SRC_URI )
+        if self.traditional:
+            self.instdestdir = "bjam\bin"
+        else:
+            self.instdestdir = "bin"
+        self.instsrcdir = PACKAGE_FULL_NAME + "-ntx86"
+
+subclass().execute()
