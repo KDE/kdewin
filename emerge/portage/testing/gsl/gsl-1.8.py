5f70012d83444863c625c766b0ec191d61cea627
Patrick Spendrin <ps_ml@gmx.de>
2007-11-11 13:50:58 +0000

    adding gsl for kspread solver to be built
    svn path=/trunk/kdesupport/kdewin32/; revision=735365

---
+++ emerge/portage/testing/gsl/gsl-1.8.py
@@ -0,0 +1,16 @@
+import base
+
+SRC_URI= """
+http://downloads.sourceforge.net/gnuwin32/gsl-1.8-bin.zip
+http://downloads.sourceforge.net/gnuwin32/gsl-1.8-lib.zip
+"""
+
+class subclass(base.baseclass):
+  def __init__(self):
+    base.baseclass.__init__( self, SRC_URI )
+    if self.traditional:
+        self.instdestdir = "win32libs"
+    else:
+        self.instdestdir = ""
+
+subclass().execute()
