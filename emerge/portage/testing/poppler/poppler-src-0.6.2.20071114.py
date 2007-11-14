4e6f2fe6ee71709047a5875d27732dc4f2ad5b8c
Patrick Spendrin <ps_ml@gmx.de>
2007-11-14 20:06:00 +0000

    removing some errors
    svn path=/trunk/kdesupport/kdewin32/; revision=736769

--- emerge/portage/testing/poppler/poppler-src-0.6.2.20071114.py
+++ emerge/portage/testing/poppler/poppler-src-0.6.2.20071114.py
@@ -1,10 +1,11 @@
 import base
 import os
 import utils
+from utils import die
 
 PACKAGE_NAME         = "poppler"
-PACKAGE_VER          = "0.62"
-PACKAGE_FULL_VER     = "0.62"
+PACKAGE_VER          = "0.6.2"
+PACKAGE_FULL_VER     = "0.6.2"
 PACKAGE_FULL_NAME    = "%s-%s" % ( PACKAGE_NAME, PACKAGE_VER )
 PACKAGE_DLL_NAME     = "poppler"
 
@@ -22,7 +23,7 @@ class subclass(base.baseclass):
     def __init__( self ):
         base.baseclass.__init__( self, "" )
 
-        def unpack( self ):
+    def unpack( self ):
         if( not base.baseclass.unpack( self ) ):
             return False
             
@@ -32,9 +33,25 @@ class subclass(base.baseclass):
               ( os.path.join( self.workdir, self.instsrcdir ), os.path.join( self.packagedir , "poppler-cmake.patch" ) )
         if not self.stayQuiet:
             print cmd
-        os.system( cmd ) or die
+        os.system( cmd ) or die( "patch" )
 
         return True
+    def compile( self ):
+        return self.kdeCompile()
 
+    def install( self ):
+        return self.kdeInstall()
+
+    def make_package( self ):
+        # auto-create both import libs with the help of pexports
+        self.stripLibs( PACKAGE_DLL_NAME )
+
+        # auto-create both import libs with the help of pexports
+        self.createImportLibs( PACKAGE_DLL_NAME )
+
+        # now do packaging with kdewin-packager
+        self.doPackaging( PACKAGE_NAME, PACKAGE_FULL_VER, True )
+
+        return True
 
 subclass().execute()
