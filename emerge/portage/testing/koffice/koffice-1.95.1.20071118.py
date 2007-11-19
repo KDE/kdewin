3d43fd5b2ef33b374e322359940f24fc439cf8a8
Patrick Spendrin <ps_ml@gmx.de>
2007-11-19 10:30:18 +0000

    mingw seems to work
    svn path=/trunk/kdesupport/kdewin32/; revision=738634

--- emerge/portage/testing/koffice/koffice-1.95.1.20071118.py
+++ emerge/portage/testing/koffice/koffice-1.95.1.20071118.py
@@ -12,28 +12,25 @@ class subclass(base.baseclass):
     def __init__( self ):
         base.baseclass.__init__( self, "" )
 
+    def kdeSvnPath( self ):
+        return "trunk/koffice"
+
     def unpack( self ):
         unp = self.kdeSvnUnpack( "trunk", "koffice" )
         # now copy the tree to workdir
-#        srcdir  = os.path.join( self.workdir, "koffice" )
-#        destdir = os.path.join( self.workdir, "kspread" )
-#        if not os.path.exists( destdir ):
-#            utils.moveSrcDirToDestDir( srcdir, destdir )
-#        else:
-#            utils.copySrcDirToDestDir( srcdir, destdir )
         return unp
 
 
     def kdeDefaultDefines( self ):
-        options = base.baseclass.kdeDefaultDefines( self )
-        options = options + "-DBUILD_karbon=OFF "
-        options = options + "-DBUILD_kdgantt=OFF "
-        options = options + "-DBUILD_kexi=OFF "
-        options = options + "-DBUILD_kivio=OFF "
-        options = options + "-DBUILD_kounavail=OFF "
-        options = options + "-DBUILD_kplato=OFF "
-        options = options + "-DBUILD_krita=OFF "
-        options = options + "-DBUILD_kword=OFF "
+#        options = base.baseclass.kdeDefaultDefines( self )
+#        options = options + "-DBUILD_karbon=OFF "
+#        options = options + "-DBUILD_kdgantt=OFF "
+#        options = options + "-DBUILD_kexi=OFF "
+#        options = options + "-DBUILD_kivio=OFF "
+#        options = options + "-DBUILD_kounavail=OFF "
+#        options = options + "-DBUILD_kplato=OFF "
+#        options = options + "-DBUILD_krita=OFF "
+#        options = options + "-DBUILD_kword=OFF "
         options = options + "-DBUILD_doc=OFF "
         return options
 
