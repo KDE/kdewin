7a3b6b5d6fbd769188527d0447262fb44eda3194
Shane King <kde@dontletsstart.com>
2007-11-21 13:03:26 +0000

    Added emerge script for amarok (fetches and configures, amarok still has compile errors currently so don't expect miracles)
    svn path=/trunk/kdesupport/kdewin32/; revision=739637

---
+++ emerge/portage/testing/amarok/amarok-2.0.20071121.py
@@ -0,0 +1,29 @@
+import base
+
+DEPEND = """
+kde/kdelibs
+kde/kdebase
+testing/ruby
+"""
+
+class subclass(base.baseclass):
+    def __init__( self ):
+        base.baseclass.__init__( self, "" )
+
+    def kdeSvnPath( self ):
+        return "trunk/extragear/multimedia/amarok"
+        
+    def unpack( self ):
+        return self.kdeSvnUnpack()
+
+    def compile( self ):
+        return self.kdeCompile()
+
+    def install( self ):
+        return self.kdeInstall()
+
+    def make_package( self ):
+        return self.doPackaging( "amarok", "2.0-SVN", True )
+
+		
+subclass().execute()
