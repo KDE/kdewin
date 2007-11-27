f57a5592f5707ca9ec6af48ee19c504558108a30
Patrick Spendrin <ps_ml@gmx.de>
2007-11-27 03:18:40 +0000

    this is more important
    svn path=/trunk/kdesupport/kdewin32/; revision=742077

--- emerge/portage/kdesupport/soprano/soprano-1.97.2.20071108.py
+++ emerge/portage/kdesupport/soprano/soprano-1.97.2.20071108.py
@@ -12,6 +12,7 @@ kdesupport/clucene-core
 class subclass(base.baseclass):
     def __init__(self):
         base.baseclass.__init__( self, "" )
+        self.instsrcdir = "soprano"
 
     def kdeSvnPath( self ):
         return "trunk/kdesupport/soprano"
@@ -28,7 +29,6 @@ class subclass(base.baseclass):
     def make_package( self ):
         if self.traditional:
             self.instdestdir = "kde"
-            self.instsrcdir = "soprano"
             return self.doPackaging( "soprano", "1.97.2-1", True )
         else:
             return self.doPackaging( "soprano", os.path.basename(sys.argv[0]).replace("soprano-", "").replace(".py", ""), True )
