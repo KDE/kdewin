d36a29fa6d0f56a2760e7fb032f94f09f163f308
Patrick Spendrin <ps_ml@gmx.de>
2007-12-13 22:55:58 +0000

    adding kdenetwork for later work on it - it might have to go to testing though - it is not very useful and tested.
    svn path=/trunk/kdesupport/kdewin32/; revision=748251

---
+++ emerge/portage/kde/kdenetwork/kdenetwork-3.97.20071212.py
@@ -0,0 +1,37 @@
+import base
+import utils
+import os
+import sys
+
+
+DEPEND = """
+virtual/base
+dev-util/win32libs
+kdesupport/kdewin32
+kdesupport/qimageblitz
+kdesupport/soprano
+kdesupport/strigi
+kde/kdebase
+"""
+
+class subclass(base.baseclass):
+    def __init__(self):
+        base.baseclass.__init__( self, "" )
+        self.instsrcdir = "kdenetwork"
+
+    def kdeSvnPath( self ):
+        return "trunk/KDE/kdenetwork"
+        
+    def unpack( self ):
+        return self.kdeSvnUnpack()
+
+    def compile( self ):
+        return self.kdeCompile()
+
+    def install( self ):
+        return self.kdeInstall()
+
+    def make_package( self ):
+        return self.doPackaging( "kdenetwork", os.path.basename(sys.argv[0]).replace("kdenetwork-", "").replace(".py", ""), True )
+
+subclass().execute()
