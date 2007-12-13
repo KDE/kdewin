d2eafcd95369e1bcaef518000b81a16648c8f4d5
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2007-12-13 21:41:35 +0000

    update to 0.3.3
    svn path=/trunk/kdesupport/kdewin32/; revision=748217

--- emerge/portage/kdesupport/kdewin32/kdewin32-0.3.3.20071213.py
+++ emerge/portage/kdesupport/kdewin32/kdewin32-0.3.3.20071213.py
@@ -27,7 +27,7 @@ class subclass(base.baseclass):
     def make_package( self ):
         if self.traditional:
             self.instdestdir = "kde"
-            return self.doPackaging( "kdewin32", "0.3.1-1", True )
+            return self.doPackaging( "kdewin32", "0.3.3-1", True )
         else:
             return self.doPackaging( "kdewin32", os.path.basename(sys.argv[0]).replace("kdewin32-", "").replace(".py", ""), True )
 subclass().execute()
