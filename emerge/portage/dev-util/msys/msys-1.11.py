0cf673e8896957b8b29053f43b9316e79ab5ed66
Patrick Spendrin <ps_ml@gmx.de>
2007-12-19 14:42:20 +0000

    adding Minimal SYStem (msys) to build at least some bash dependent stuff.
    svn path=/trunk/kdesupport/kdewin32/; revision=750539

---
+++ emerge/portage/dev-util/msys/msys-1.11.py
@@ -0,0 +1,15 @@
+import base
+import utils
+from utils import die
+import os
+
+SRC_URI = """
+http://heanet.dl.sourceforge.net/mingw/msysCORE-1.0.11-2007.01.19-1.tar.bz2
+"""
+
+class subclass(base.baseclass):
+    def __init__( self ):
+        base.baseclass.__init__( self, SRC_URI )
+        self.instdestdir = "msys"
+	
+subclass().execute()
