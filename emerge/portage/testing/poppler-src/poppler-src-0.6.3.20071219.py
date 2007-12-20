f1eaf53998508e15d437c175acb47be36145c720
Patrick Spendrin <ps_ml@gmx.de>
2007-12-20 23:55:42 +0000

    making poppler work in the end (hopefully)
    svn path=/trunk/kdesupport/kdewin32/; revision=751058

--- emerge/portage/testing/poppler-src/poppler-src-0.6.3.20071219.py
+++ emerge/portage/testing/poppler-src/poppler-src-0.6.3.20071219.py
@@ -9,7 +9,10 @@ PACKAGE_FULL_VER     = "0.6.3"
 PACKAGE_FULL_NAME    = "%s-%s" % ( PACKAGE_NAME, PACKAGE_VER )
 PACKAGE_DLL_NAME     = "poppler"
 
-SRC_URI = """http://poppler.freedesktop.org/poppler-0.6.3.tar.gz"""
+SRC_URI = """
+http://poppler.freedesktop.org/poppler-0.6.3.tar.gz
+http://poppler.freedesktop.org/poppler-data-0.2.0.tar.gz
+"""
 
 DEPEND = """
 gnuwin32/patch
@@ -37,9 +40,11 @@ class subclass(base.baseclass):
         if utils.verbose() >= 1:
             print cmd
         self.system( cmd ) or die( "patch" )
-
         return True
+        
+        
     def compile( self ):
+        self.kdeCustomDefines = "-DBUILD_QT4_TESTS=ON"
         return self.kdeCompile()
 
     def install( self ):
