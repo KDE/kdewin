52be39d690fe3c994353eeecd111a5e9ea4c109c
Patrick Spendrin <ps_ml@gmx.de>
2007-12-29 01:58:41 +0000

    it seems there is no real way to make if + set working in the right way - now use a custom target instead
    svn path=/trunk/kdesupport/kdewin32/; revision=754098

--- emerge/kdeenv.bat
+++ emerge/kdeenv.bat
@@ -2,36 +2,53 @@
 rem    this file sets some environment variables that are needed
 rem    for finding programs and libraries etc.
 rem    by Holger Schroeder <schroder@kde.org>
+rem    by Patrick Spendrin <ps_ml@gmx.de>
 
 rem    you should copy kdesettings-example.bat to ..\etc\kdesettings.bat
 rem    and adapt it to your needs (see that file for more info)
 
+rem    this file should contain all path settings - and provide thus an environment
+rem    to build and run kde programs
+rem    this file sources the kdesettings.bat file automatically
+
 call ..\etc\kdesettings.bat
 
-set PATH=%PATH%;%KDEROOT%\emerge\bin
-if /i %KDECOMPILER% == mingw (
-    set PATH=%PATH%;%KDEROOT%\mingw\bin
-)
+set PATH=%KDEROOT%\emerge\bin;%PATH%
 
-if /i %directory_layout% == installer (
-    set PATH=%KDEROOT%\bin;%PATH%
-    set PATH=%KDEROOT%\lib;%PATH%
-    set KDEDIRS=%KDEROOT%
+if %KDECOMPILER% == mingw ( set PATH=%PATH%;%KDEROOT%\mingw\bin )
+
+if %directory_layout% == installer ( 
+    call :path-installer
 ) else (
-    set PATH=%PATH%;%KDEROOT%\gnuwin32\bin
-    set PATH=%PATH%;%KDEROOT%\subversion\bin
-    set PATH=%PATH%;%KDEROOT%\qt\bin
-    set PATH=%PATH%;%KDEROOT%\qt\lib
-    set PATH=%PATH%;%KDEROOT%\cmake\bin
-    set PATH=%PATH%;%KDEROOT%\kdewin32\bin
-    set PATH=%PATH%;%KDEROOT%\win32libs\bin
-    set PATH=%PATH%;%KDEROOT%\kde\bin
-    set PATH=%PATH%;%KDEROOT%\kde\lib
-    set PATH=%PATH%;%KDEROOT%\dbus\bin
-    set PATH=%PATH%;%KDEROOT%\mc
-    set PATH=%PATH%;%KDEROOT%\perl\bin
-    set PATH=%PATH%;%KDEROOT%\strigi\bin
-    set KDEDIRS=%KDEROOT%\kde
+    call :path-traditional
 )
-
 cmd /e:on /K "cd %KDEROOT%"
+goto :eof
+
+:: installer layout: only one installation directory - which serves as installation root for all packages
+:: its subfolder bin contains all the executables and should contain all dynamic libraries
+:: putting the lib directory into the path shouldn't be needed, it is included though for convenience
+:path-installer
+    set PATH=%KDEROOT%\lib;%PATH%   
+    set PATH=%KDEROOT%\bin;%PATH%   
+    set KDEDIRS=%KDEROOT%
+    goto :eof
+
+:: traditional layout: depending on the categories, there exist subfolders in %KDEROOT%: gnuwin32 for the gnuwin32 packages, qt
+:: for qt headers, libraries and of course
+:path-traditional
+    set PATH=%KDEROOT%\gnuwin32\bin;%PATH%
+    set PATH=%KDEROOT%\subversion\bin;%PATH%
+    set PATH=%KDEROOT%\qt\bin;%PATH%
+    set PATH=%KDEROOT%\cmake\bin;%PATH%
+    set PATH=%KDEROOT%\kdewin32\bin;%PATH%
+    set PATH=%KDEROOT%\win32libs\bin;%PATH%
+    set PATH=%KDEROOT%\kde\bin;%PATH%
+    set PATH=%KDEROOT%\dbus\bin;%PATH%
+    set PATH=%KDEROOT%\mc;%PATH%
+    set PATH=%KDEROOT%\perl\bin;%PATH%
+    set PATH=%KDEROOT%\strigi\bin;%PATH%
+    set PATH=%KDEROOT%\kde\lib;%PATH%
+    set PATH=%KDEROOT%\qt\lib;%PATH%
+    set KDEDIRS=%KDEROOT%\emerge
+    goto :eof
