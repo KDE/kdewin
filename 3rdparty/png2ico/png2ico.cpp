18727875ab126f9daccb44fdccdf500cee182983
Patrick Spendrin <ps_ml@gmx.de>
2007-12-30 19:04:38 +0000

    for chehrlic: fix install winposix_export.h
    svn path=/trunk/kdesupport/kdewin32/; revision=754786

--- 3rdparty/png2ico/png2ico.cpp
+++ 3rdparty/png2ico/png2ico.cpp
@@ -220,7 +220,7 @@ bool convertToIndexed(png_data& img, bool hasAlpha)
   
   if (mapQuadToPalEntry.size()*img.requested_colors>slow_reduction_warn_threshold)
   {
-    fprintf(stdout,"Please be patient. My color reduction algorithm is really slow.\n");
+    //fprintf(stdout,"Please be patient. My color reduction algorithm is really slow.\n");
   };
   
   //Now fill up the palette with colors from the image by repeatedly picking the
@@ -459,8 +459,6 @@ int main(int argc, char* argv[])
   
   static int numColors=256; //static to get rid of longjmp() clobber warning
   static const char* outfileName=NULL;
-  bool createRcFile = false;
-  static const char* rcFileName=NULL;
   
   //i is static because used in a setjmp() block
   for (static int i=1; i<argc; ++i)
@@ -482,19 +480,7 @@ int main(int argc, char* argv[])
       };
       numColors=num;
       continue;
-    }
-    else if (strcmp(argv[i],"--rcfile")==0)
-    {
-      ++i;
-      if (i>=argc)
-      {
-        fprintf(stderr,"no filename given for rcfile\n");
-        exit(1);
-      };
-      createRcFile = true;
-      rcFileName = argv[i];
-      i++;
-    }
+    };
     
     if (outfileName==NULL) { outfileName=argv[i]; continue; };
     
@@ -670,15 +656,6 @@ int main(int argc, char* argv[])
     png_destroy_info_struct(it->png_ptr, &it->end_info);
     png_destroy_read_struct(&it->png_ptr, 0, 0);
   }
-
-  // create .rc - file 
-  if (createRcFile) {
-      FILE* rcfile=fopen(rcFileName,"wb");
-      if (rcfile==NULL) {perror(rcFileName); exit(1);};
-      fprintf(rcfile,"IDI_ICON1        ICON        DISCARDABLE    \"%s\"\n",outfileName);
-      fclose(rcfile);
-  }
-  return 0;
 };
 
 
