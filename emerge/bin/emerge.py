8a87b4640b21e238b2b992b24191ed662b0bdf78
Patrick Spendrin <ps_ml@gmx.de>
2008-01-07 23:51:44 +0000

    fix error: use sys.path instead of the useless os.environ["PYTHONPATH"]
    svn path=/trunk/kdesupport/kdewin32/; revision=758472

--- emerge/bin/emerge.py
+++ emerge/bin/emerge.py
@@ -210,8 +210,7 @@ if not os.getenv( "CMAKE_FIND_PREFIX" ) == None:
     
 
 # adding emerge/bin to find base.py and gnuwin32.py etc.
-os.environ["PYTHONPATH"] = os.getenv( "PYTHONPATH" ) + ";" +\
-                           os.path.join( os.getcwd(), os.path.dirname( executableName ) )
+sys.path.append( os.path.join( os.getcwd(), os.path.dirname( executableName ) ) )
 
 deplist = []
 if packageName:
