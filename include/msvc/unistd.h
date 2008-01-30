9a429aef07ff3ce77723348e4628ffa9855eae4d
Jarosław Staniek <staniek@kde.org>
2008-01-30 11:52:05 +0000

    reverted 768534 - found other workaround in strigi
    svn path=/trunk/kdesupport/kdewin32/; revision=768575

--- include/msvc/unistd.h
+++ include/msvc/unistd.h
@@ -114,7 +114,6 @@ KDEWIN32_EXPORT void sleep(unsigned int sec);
 KDEWIN32_EXPORT int setreuid(uid_t ruid, uid_t euid);
 
 KDEWIN32_EXPORT int mkstemps(char* _template, int suffix_len);
-#define HAVE_MKSTEMPS
 
 KDEWIN32_EXPORT int initgroups(const char *name, int basegid);
 
@@ -123,10 +122,8 @@ KDEWIN32_EXPORT int initgroups(const char *name, int basegid);
 KDEWIN32_EXPORT int seteuid(uid_t euid);
 
 KDEWIN32_EXPORT int mkstemp (char* _template);
-#define HAVE_MKSTEMP
 
 KDEWIN32_EXPORT char* mkdtemp (char* _template);
-#define HAVE_MKDTEMP
 
 KDEWIN32_EXPORT int revoke(const char *tty);
 
