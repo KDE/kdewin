08762e7c8e1b00d69e41e3f30fa9aa745136230c
Patrick von Reth <patrick.vonreth@gmail.com>
2010-04-25 23:07:24 +0000

    improved prevention of winsock.h header, uts now still possible to include it manually but its not included by default
    svn path=/trunk/kdesupport/kdewin/; revision=1118818

--- include/mingw/windows.h
+++ include/mingw/windows.h
@@ -1,30 +1,31 @@
-/* This file is part of the KDE project
+/* This file is part of the KDE libraries
    Copyright (C) 2010  Ralf Habacker <ralf.habacker@freenet.de>
+   Copyright (C) 2010 Patrick von Reth <patrick.vonreth@gmail.com>
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
    License as published by the Free Software Foundation; either
    version 2 of the License, or (at your option) any later version.
 
-   This program is distributed in the hope that it will be useful,
+   This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Library General Public License for more details.
 
    You should have received a copy of the GNU Library General Public License
-   along with this program; see the file COPYING.  If not, write to
+   along with this library; see the file COPYING.LIB.  If not, write to
    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
    Boston, MA 02110-1301, USA.
 */
 
-#ifndef _KDEWIN_WINDOWS_H
-#define _KDEWIN_WINDOWS_H
+#ifndef KDEWIN_WINDOWS_H
+#define KDEWIN_WINDOWS_H
 
-#ifdef __MINGW64__
-//  avoid inclusion of winsock 1 api 
-#define _WINSOCKAPI_
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
 #endif
+
 #include <../include/windows.h>
 
-
-#endif /* _KDEWIN_WINDOWS_H */
+#endif /* KDEWIN_WINDOWS_H */
