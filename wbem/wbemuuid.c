b77b02f3ea861e342f5a6f29a7efc5e23c0210d5
Ralf Habacker <ralf.habacker@freenet.de>
2010-04-28 13:29:52 +0000

    only export real defined instances
    svn path=/trunk/kdesupport/kdewin/; revision=1120146

--- wbem/wbemuuid.c
+++ wbem/wbemuuid.c
@@ -18,259 +18,266 @@
 */
 
 /*
-  IID and CLSID instances for wbemuuid.lib  
+  IID and CLSID instances for wbemuuid.lib 
+  
+  not complete yet
+  
+  only instances containing real values should be exported by 
+  replacing TEMPLATE through EXTERN_C
 */  
 
 #include <guiddef.h>
 
 #define EXTERN_C 
 
-/* PassiveSink */
-EXTERN_C const IID CLSID_PseudoSink;
+#define TEMPLATE static
 
-EXTERN_C const IID IID_IWbemDecoupledEventSink;
+/* PassiveSink */
+TEMPLATE const IID CLSID_PseudoSink;
+
+TEMPLATE const IID IID_IWbemDecoupledEventSink;
 
 /* WbemUtilities_v1 */
-EXTERN_C const IID CLSID_WbemDefPath;
-EXTERN_C const IID CLSID_WbemQuery;
+TEMPLATE const IID CLSID_WbemDefPath;
+TEMPLATE const IID CLSID_WbemQuery;
 
-EXTERN_C const IID IID_IWbemPath;
-EXTERN_C const IID IID_IWbemPathKeyList;
-EXTERN_C const IID IID_IWbemQuery;
+TEMPLATE const IID IID_IWbemPath;
+TEMPLATE const IID IID_IWbemPathKeyList;
+TEMPLATE const IID IID_IWbemQuery;
 
 /* WMIEXTENSION */
-EXTERN_C const IID CLSID_WMIExtension;
+TEMPLATE const IID CLSID_WMIExtension;
 
-EXTERN_C const IID IID_IWMIExtension;
+TEMPLATE const IID IID_IWMIExtension;
 
 /* WbemScripting */
-EXTERN_C const IID CLSID_SWbemDateTime;
-EXTERN_C const IID CLSID_SWbemEventSource;
-EXTERN_C const IID CLSID_SWbemLocator;
-EXTERN_C const IID CLSID_SWbemNamedValue;
-EXTERN_C const IID CLSID_SWbemNamedValueSet;
-EXTERN_C const IID CLSID_SWbemMethod;
-EXTERN_C const IID CLSID_SWbemMethodSet;
-EXTERN_C const IID CLSID_SWbemObject;
-EXTERN_C const IID CLSID_SWbemObjectEx;
-EXTERN_C const IID CLSID_SWbemObjectPath;
-EXTERN_C const IID CLSID_SWbemObjectSet;
-EXTERN_C const IID CLSID_SWbemPrivilege;
-EXTERN_C const IID CLSID_SWbemPrivilegeSet;
-EXTERN_C const IID CLSID_SWbemProperty;
-EXTERN_C const IID CLSID_SWbemPropertySet;
-EXTERN_C const IID CLSID_SWbemQualifier;
-EXTERN_C const IID CLSID_SWbemQualifierSet;
-EXTERN_C const IID CLSID_SWbemLastError;
-EXTERN_C const IID CLSID_SWbemRefreshableItem;
-EXTERN_C const IID CLSID_SWbemRefresher;
-EXTERN_C const IID CLSID_SWbemSecurity;
-EXTERN_C const IID CLSID_SWbemServices;
-EXTERN_C const IID CLSID_SWbemServicesEx;
-EXTERN_C const IID CLSID_SWbemSink;
+TEMPLATE const IID CLSID_SWbemDateTime;
+TEMPLATE const IID CLSID_SWbemEventSource;
+TEMPLATE const IID CLSID_SWbemLocator;
+TEMPLATE const IID CLSID_SWbemNamedValue;
+TEMPLATE const IID CLSID_SWbemNamedValueSet;
+TEMPLATE const IID CLSID_SWbemMethod;
+TEMPLATE const IID CLSID_SWbemMethodSet;
+TEMPLATE const IID CLSID_SWbemObject;
+TEMPLATE const IID CLSID_SWbemObjectEx;
+TEMPLATE const IID CLSID_SWbemObjectPath;
+TEMPLATE const IID CLSID_SWbemObjectSet;
+TEMPLATE const IID CLSID_SWbemPrivilege;
+TEMPLATE const IID CLSID_SWbemPrivilegeSet;
+TEMPLATE const IID CLSID_SWbemProperty;
+TEMPLATE const IID CLSID_SWbemPropertySet;
+TEMPLATE const IID CLSID_SWbemQualifier;
+TEMPLATE const IID CLSID_SWbemQualifierSet;
+TEMPLATE const IID CLSID_SWbemLastError;
+TEMPLATE const IID CLSID_SWbemRefreshableItem;
+TEMPLATE const IID CLSID_SWbemRefresher;
+TEMPLATE const IID CLSID_SWbemSecurity;
+TEMPLATE const IID CLSID_SWbemServices;
+TEMPLATE const IID CLSID_SWbemServicesEx;
+TEMPLATE const IID CLSID_SWbemSink;
 
-EXTERN_C const IID DIID_ISWbemSinkEvents;
+TEMPLATE const IID DIID_ISWbemSinkEvents;
 
-EXTERN_C const IID IID_ISWbemDateTime;
-EXTERN_C const IID IID_ISWbemEventSource;
-EXTERN_C const IID IID_ISWbemLastError;
-EXTERN_C const IID IID_ISWbemLocator;
-EXTERN_C const IID IID_ISWbemNamedValue;
-EXTERN_C const IID IID_ISWbemNamedValueSet;
-EXTERN_C const IID IID_ISWbemMethod;
-EXTERN_C const IID IID_ISWbemMethodSet;
-EXTERN_C const IID IID_ISWbemObject;
-EXTERN_C const IID IID_ISWbemObjectEx;
-EXTERN_C const IID IID_ISWbemObjectPath;
-EXTERN_C const IID IID_ISWbemObjectSet;
-EXTERN_C const IID IID_ISWbemPrivilege;
-EXTERN_C const IID IID_ISWbemPrivilegeSet;
-EXTERN_C const IID IID_ISWbemProperty;
-EXTERN_C const IID IID_ISWbemPropertySet;
-EXTERN_C const IID IID_ISWbemQualifier;
-EXTERN_C const IID IID_ISWbemQualifierSet;
-EXTERN_C const IID IID_ISWbemRefresher;
-EXTERN_C const IID IID_ISWbemRefreshableItem;
-EXTERN_C const IID IID_ISWbemSecurity;
-EXTERN_C const IID IID_ISWbemServices;
-EXTERN_C const IID IID_ISWbemServicesEx;
-EXTERN_C const IID IID_ISWbemSink;
+TEMPLATE const IID IID_ISWbemDateTime;
+TEMPLATE const IID IID_ISWbemEventSource;
+TEMPLATE const IID IID_ISWbemLastError;
+TEMPLATE const IID IID_ISWbemLocator;
+TEMPLATE const IID IID_ISWbemNamedValue;
+TEMPLATE const IID IID_ISWbemNamedValueSet;
+TEMPLATE const IID IID_ISWbemMethod;
+TEMPLATE const IID IID_ISWbemMethodSet;
+TEMPLATE const IID IID_ISWbemObject;
+TEMPLATE const IID IID_ISWbemObjectEx;
+TEMPLATE const IID IID_ISWbemObjectPath;
+TEMPLATE const IID IID_ISWbemObjectSet;
+TEMPLATE const IID IID_ISWbemPrivilege;
+TEMPLATE const IID IID_ISWbemPrivilegeSet;
+TEMPLATE const IID IID_ISWbemProperty;
+TEMPLATE const IID IID_ISWbemPropertySet;
+TEMPLATE const IID IID_ISWbemQualifier;
+TEMPLATE const IID IID_ISWbemQualifierSet;
+TEMPLATE const IID IID_ISWbemRefresher;
+TEMPLATE const IID IID_ISWbemRefreshableItem;
+TEMPLATE const IID IID_ISWbemSecurity;
+TEMPLATE const IID IID_ISWbemServices;
+TEMPLATE const IID IID_ISWbemServicesEx;
+TEMPLATE const IID IID_ISWbemSink;
 
 /* WbemTransports_v1 */
-EXTERN_C const IID CLSID_WbemDCOMTransport;
-EXTERN_C const IID CLSID_WbemLevel1Login;
-EXTERN_C const IID CLSID_WbemLocalAddrRes;
-EXTERN_C const IID CLSID_WbemUninitializedClassObject;
+TEMPLATE const IID CLSID_WbemDCOMTransport;
+TEMPLATE const IID CLSID_WbemLevel1Login;
+TEMPLATE const IID CLSID_WbemLocalAddrRes;
+TEMPLATE const IID CLSID_WbemUninitializedClassObject;
 
-EXTERN_C const IID IID_IWbemAddressResolution;
-EXTERN_C const IID IID_IWbemClientConnectionTransport;
-EXTERN_C const IID IID_IWbemClientTransport;
-EXTERN_C const IID IID_IWbemConnectorLogin;
-EXTERN_C const IID IID_IWbemConstructClassObject;
-EXTERN_C const IID IID_IWbemLevel1Login;
-EXTERN_C const IID IID_IWbemTransport;
+TEMPLATE const IID IID_IWbemAddressResolution;
+TEMPLATE const IID IID_IWbemClientConnectionTransport;
+TEMPLATE const IID IID_IWbemClientTransport;
+TEMPLATE const IID IID_IWbemConnectorLogin;
+TEMPLATE const IID IID_IWbemConstructClassObject;
+TEMPLATE const IID IID_IWbemLevel1Login;
+TEMPLATE const IID IID_IWbemTransport;
 
 /* WbemClient_v1 */
-EXTERN_C const IID IID_IEnumWbemClassObject;
-EXTERN_C const IID IID_IUnsecuredApartment;
-EXTERN_C const IID IID_IWbemClassObject;
+TEMPLATE const IID IID_IEnumWbemClassObject;
+TEMPLATE const IID IID_IUnsecuredApartment;
+TEMPLATE const IID IID_IWbemClassObject;
 EXTERN_C const IID IID_IWbemLocator = {
     0xDC12A687,
     0x737F, 0x11CF,
     0x88,0x4D,0x00,0xAA,0x00,0x4B,0x2E,0x24
 };
-EXTERN_C const IID IID_IWbemBackupRestore;
-EXTERN_C const IID IID_IWbemBackupRestoreEx;
-EXTERN_C const IID IID_IWbemConfigureRefresher;
-EXTERN_C const IID IID_IWbemCallResult;
-EXTERN_C const IID IID_IWbemContext;
-EXTERN_C const IID IID_IWbemHiPerfEnum;
-EXTERN_C const IID IID_IWbemObjectAccess;
-EXTERN_C const IID IID_IWbemObjectSink;
-EXTERN_C const IID IID_IWbemQualifierSet;
-EXTERN_C const IID IID_IWbemRefresher;
-EXTERN_C const IID IID_IWbemServices;
-EXTERN_C const IID IID_IWbemStatusCodeText;
-EXTERN_C const IID IID_IWbemUnsecuredApartment;
+TEMPLATE const IID IID_IWbemBackupRestore;
+TEMPLATE const IID IID_IWbemBackupRestoreEx;
+TEMPLATE const IID IID_IWbemConfigureRefresher;
+TEMPLATE const IID IID_IWbemCallResult;
+TEMPLATE const IID IID_IWbemContext;
+TEMPLATE const IID IID_IWbemHiPerfEnum;
+TEMPLATE const IID IID_IWbemObjectAccess;
+TEMPLATE const IID IID_IWbemObjectSink;
+TEMPLATE const IID IID_IWbemQualifierSet;
+TEMPLATE const IID IID_IWbemRefresher;
+TEMPLATE const IID IID_IWbemServices;
+TEMPLATE const IID IID_IWbemStatusCodeText;
+TEMPLATE const IID IID_IWbemUnsecuredApartment;
 
 /* PassiveSink */
-EXTERN_C const CLSID CLSID_PseudoSink;
+TEMPLATE const CLSID CLSID_PseudoSink;
 
-EXTERN_C const IID IID_IWbemDecoupledEventSink;
+TEMPLATE const IID IID_IWbemDecoupledEventSink;
 
 /* WbemUtilities_v1 */
-EXTERN_C const CLSID CLSID_WbemDefPath;
-EXTERN_C const CLSID CLSID_WbemQuery;
+TEMPLATE const CLSID CLSID_WbemDefPath;
+TEMPLATE const CLSID CLSID_WbemQuery;
 
-EXTERN_C const IID IID_IWbemPathKeyList;
-EXTERN_C const IID IID_IWbemPath;
-EXTERN_C const IID IID_IWbemQuery;
+TEMPLATE const IID IID_IWbemPathKeyList;
+TEMPLATE const IID IID_IWbemPath;
+TEMPLATE const IID IID_IWbemQuery;
 
 /* WMIEXTENSION */
-EXTERN_C const CLSID CLSID_WMIExtension;
+TEMPLATE const CLSID CLSID_WMIExtension;
 
-EXTERN_C const IID IID_IWMIExtension;
+TEMPLATE const IID IID_IWMIExtension;
 
 /* WbemScripting */
-EXTERN_C const CLSID CLSID_SWbemDateTime;
-EXTERN_C const CLSID CLSID_SWbemEventSource;
-EXTERN_C const CLSID CLSID_SWbemLastError;
-EXTERN_C const CLSID CLSID_SWbemLocator;
-EXTERN_C const CLSID CLSID_SWbemNamedValue;
-EXTERN_C const CLSID CLSID_SWbemNamedValueSet;
-EXTERN_C const CLSID CLSID_SWbemMethod;
-EXTERN_C const CLSID CLSID_SWbemMethodSet;
-EXTERN_C const CLSID CLSID_SWbemQualifier;
-EXTERN_C const CLSID CLSID_SWbemQualifierSet;
-EXTERN_C const CLSID CLSID_SWbemPrivilege;
-EXTERN_C const CLSID CLSID_SWbemPrivilegeSet;
-EXTERN_C const CLSID CLSID_SWbemRefreshableItem;
-EXTERN_C const CLSID CLSID_SWbemProperty;
-EXTERN_C const CLSID CLSID_SWbemPropertySet;
-EXTERN_C const CLSID CLSID_SWbemObject;
-EXTERN_C const CLSID CLSID_SWbemObjectEx;
-EXTERN_C const CLSID CLSID_SWbemObjectPath;
-EXTERN_C const CLSID CLSID_SWbemObjectSet;
-EXTERN_C const CLSID CLSID_SWbemRefresher;
-EXTERN_C const CLSID CLSID_SWbemServices;
-EXTERN_C const CLSID CLSID_SWbemServicesEx;
-EXTERN_C const CLSID CLSID_SWbemSink;
-EXTERN_C const CLSID CLSID_SWbemSecurity;
+TEMPLATE const CLSID CLSID_SWbemDateTime;
+TEMPLATE const CLSID CLSID_SWbemEventSource;
+TEMPLATE const CLSID CLSID_SWbemLastError;
+TEMPLATE const CLSID CLSID_SWbemLocator;
+TEMPLATE const CLSID CLSID_SWbemNamedValue;
+TEMPLATE const CLSID CLSID_SWbemNamedValueSet;
+TEMPLATE const CLSID CLSID_SWbemMethod;
+TEMPLATE const CLSID CLSID_SWbemMethodSet;
+TEMPLATE const CLSID CLSID_SWbemQualifier;
+TEMPLATE const CLSID CLSID_SWbemQualifierSet;
+TEMPLATE const CLSID CLSID_SWbemPrivilege;
+TEMPLATE const CLSID CLSID_SWbemPrivilegeSet;
+TEMPLATE const CLSID CLSID_SWbemRefreshableItem;
+TEMPLATE const CLSID CLSID_SWbemProperty;
+TEMPLATE const CLSID CLSID_SWbemPropertySet;
+TEMPLATE const CLSID CLSID_SWbemObject;
+TEMPLATE const CLSID CLSID_SWbemObjectEx;
+TEMPLATE const CLSID CLSID_SWbemObjectPath;
+TEMPLATE const CLSID CLSID_SWbemObjectSet;
+TEMPLATE const CLSID CLSID_SWbemRefresher;
+TEMPLATE const CLSID CLSID_SWbemServices;
+TEMPLATE const CLSID CLSID_SWbemServicesEx;
+TEMPLATE const CLSID CLSID_SWbemSink;
+TEMPLATE const CLSID CLSID_SWbemSecurity;
 
-EXTERN_C const IID DIID_ISWbemSinkEvents;
+TEMPLATE const IID DIID_ISWbemSinkEvents;
 
-EXTERN_C const IID IID_ISWbemDateTime;
-EXTERN_C const IID IID_ISWbemEventSource;
-EXTERN_C const IID IID_ISWbemLastError;
-EXTERN_C const IID IID_ISWbemLocator;
-EXTERN_C const IID IID_ISWbemNamedValue;
-EXTERN_C const IID IID_ISWbemNamedValueSet;
-EXTERN_C const IID IID_ISWbemMethod;
-EXTERN_C const IID IID_ISWbemMethodSet;
-EXTERN_C const IID IID_ISWbemObject;
-EXTERN_C const IID IID_ISWbemObjectEx;
-EXTERN_C const IID IID_ISWbemObjectSet;
-EXTERN_C const IID IID_ISWbemObjectPath;
-EXTERN_C const IID IID_ISWbemPrivilege;
-EXTERN_C const IID IID_ISWbemPrivilegeSet;
-EXTERN_C const IID IID_ISWbemProperty;
-EXTERN_C const IID IID_ISWbemPropertySet;
-EXTERN_C const IID IID_ISWbemQualifier;
-EXTERN_C const IID IID_ISWbemQualifierSet;
-EXTERN_C const IID IID_ISWbemRefresher;
-EXTERN_C const IID IID_ISWbemRefreshableItem;
-EXTERN_C const IID IID_ISWbemSecurity;
-EXTERN_C const IID IID_ISWbemServices;
-EXTERN_C const IID IID_ISWbemServicesEx;
-EXTERN_C const IID IID_ISWbemSink;
+TEMPLATE const IID IID_ISWbemDateTime;
+TEMPLATE const IID IID_ISWbemEventSource;
+TEMPLATE const IID IID_ISWbemLastError;
+TEMPLATE const IID IID_ISWbemLocator;
+TEMPLATE const IID IID_ISWbemNamedValue;
+TEMPLATE const IID IID_ISWbemNamedValueSet;
+TEMPLATE const IID IID_ISWbemMethod;
+TEMPLATE const IID IID_ISWbemMethodSet;
+TEMPLATE const IID IID_ISWbemObject;
+TEMPLATE const IID IID_ISWbemObjectEx;
+TEMPLATE const IID IID_ISWbemObjectSet;
+TEMPLATE const IID IID_ISWbemObjectPath;
+TEMPLATE const IID IID_ISWbemPrivilege;
+TEMPLATE const IID IID_ISWbemPrivilegeSet;
+TEMPLATE const IID IID_ISWbemProperty;
+TEMPLATE const IID IID_ISWbemPropertySet;
+TEMPLATE const IID IID_ISWbemQualifier;
+TEMPLATE const IID IID_ISWbemQualifierSet;
+TEMPLATE const IID IID_ISWbemRefresher;
+TEMPLATE const IID IID_ISWbemRefreshableItem;
+TEMPLATE const IID IID_ISWbemSecurity;
+TEMPLATE const IID IID_ISWbemServices;
+TEMPLATE const IID IID_ISWbemServicesEx;
+TEMPLATE const IID IID_ISWbemSink;
 
 /* WbemTransports_v1 */
-EXTERN_C const CLSID CLSID_WbemDCOMTransport;
-EXTERN_C const CLSID CLSID_WbemLevel1Login;
-EXTERN_C const CLSID CLSID_WbemLocalAddrRes;
-EXTERN_C const CLSID CLSID_WbemUninitializedClassObject;
+TEMPLATE const CLSID CLSID_WbemDCOMTransport;
+TEMPLATE const CLSID CLSID_WbemLevel1Login;
+TEMPLATE const CLSID CLSID_WbemLocalAddrRes;
+TEMPLATE const CLSID CLSID_WbemUninitializedClassObject;
 
-EXTERN_C const IID IID_IWbemAddressResolution;
-EXTERN_C const IID IID_IWbemClientTransport;
-EXTERN_C const IID IID_IWbemClientConnectionTransport;
-EXTERN_C const IID IID_IWbemConnectorLogin;
-EXTERN_C const IID IID_IWbemConstructClassObject;
-EXTERN_C const IID IID_IWbemLevel1Login;
-EXTERN_C const IID IID_IWbemTransport;
+TEMPLATE const IID IID_IWbemAddressResolution;
+TEMPLATE const IID IID_IWbemClientTransport;
+TEMPLATE const IID IID_IWbemClientConnectionTransport;
+TEMPLATE const IID IID_IWbemConnectorLogin;
+TEMPLATE const IID IID_IWbemConstructClassObject;
+TEMPLATE const IID IID_IWbemLevel1Login;
+TEMPLATE const IID IID_IWbemTransport;
 
 /* WbemClient_v1 */
-EXTERN_C const CLSID CLSID_MofCompiler;
-EXTERN_C const CLSID CLSID_UnsecuredApartment;
-EXTERN_C const CLSID CLSID_WbemBackupRestore;
-EXTERN_C const CLSID CLSID_WbemContext;
-EXTERN_C const CLSID CLSID_WbemClassObject;
+TEMPLATE const CLSID CLSID_MofCompiler;
+TEMPLATE const CLSID CLSID_UnsecuredApartment;
+TEMPLATE const CLSID CLSID_WbemBackupRestore;
+TEMPLATE const CLSID CLSID_WbemContext;
+TEMPLATE const CLSID CLSID_WbemClassObject;
 EXTERN_C const CLSID CLSID_WbemLocator = {
     0x4590F811, 
     0x1D3A,0x11D0,
     0x89,0x1F,0x00,0xAA,0x00,0x4B,0x2E,0x24
 };
-EXTERN_C const CLSID CLSID_WbemObjectTextSrc;
-EXTERN_C const CLSID CLSID_WbemRefresher;
-EXTERN_C const CLSID CLSID_WbemStatusCodeText;
+TEMPLATE const CLSID CLSID_WbemObjectTextSrc;
+TEMPLATE const CLSID CLSID_WbemRefresher;
+TEMPLATE const CLSID CLSID_WbemStatusCodeText;
 
-EXTERN_C const IID IID_IEnumWbemClassObject;
-EXTERN_C const IID IID_IMofCompiler;
-EXTERN_C const IID IID_IUnsecuredApartment;
-EXTERN_C const IID IID_IWbemBackupRestore;
-EXTERN_C const IID IID_IWbemBackupRestoreEx;
-EXTERN_C const IID IID_IWbemCallResult;
-EXTERN_C const IID IID_IWbemClassObject;
-EXTERN_C const IID IID_IWbemConfigureRefresher;
-EXTERN_C const IID IID_IWbemContext;
-EXTERN_C const IID IID_IWbemHiPerfEnum;
-EXTERN_C const IID IID_IWbemLocator;
-EXTERN_C const IID IID_IWbemObjectAccess;
-EXTERN_C const IID IID_IWbemObjectSink;
-EXTERN_C const IID IID_IWbemObjectTextSrc;
-EXTERN_C const IID IID_IWbemQualifierSet;
-EXTERN_C const IID IID_IWbemServices;
-EXTERN_C const IID IID_IWbemShutdown;
-EXTERN_C const IID IID_IWbemStatusCodeText;
-EXTERN_C const IID IID_IWbemRefresher;
-EXTERN_C const IID IID_IWbemUnsecuredApartment;
+TEMPLATE const IID IID_IEnumWbemClassObject;
+TEMPLATE const IID IID_IMofCompiler;
+TEMPLATE const IID IID_IUnsecuredApartment;
+TEMPLATE const IID IID_IWbemBackupRestore;
+TEMPLATE const IID IID_IWbemBackupRestoreEx;
+TEMPLATE const IID IID_IWbemCallResult;
+TEMPLATE const IID IID_IWbemClassObject;
+TEMPLATE const IID IID_IWbemConfigureRefresher;
+TEMPLATE const IID IID_IWbemContext;
+TEMPLATE const IID IID_IWbemHiPerfEnum;
+TEMPLATE const IID IID_IWbemLocator;
+TEMPLATE const IID IID_IWbemObjectAccess;
+TEMPLATE const IID IID_IWbemObjectSink;
+TEMPLATE const IID IID_IWbemObjectTextSrc;
+TEMPLATE const IID IID_IWbemQualifierSet;
+TEMPLATE const IID IID_IWbemServices;
+TEMPLATE const IID IID_IWbemShutdown;
+TEMPLATE const IID IID_IWbemStatusCodeText;
+TEMPLATE const IID IID_IWbemRefresher;
+TEMPLATE const IID IID_IWbemUnsecuredApartment;
 
 /* WbemProviders_v1 */
-EXTERN_C const CLSID CLSID_WbemAdministrativeLocator;
-EXTERN_C const CLSID CLSID_WbemAuthenticatedLocator;
-EXTERN_C const CLSID CLSID_WbemDecoupledRegistrar;
-EXTERN_C const CLSID CLSID_WbemDecoupledBasicEventProvider;
-EXTERN_C const CLSID CLSID_WbemUnauthenticatedLocator;
+TEMPLATE const CLSID CLSID_WbemAdministrativeLocator;
+TEMPLATE const CLSID CLSID_WbemAuthenticatedLocator;
+TEMPLATE const CLSID CLSID_WbemDecoupledRegistrar;
+TEMPLATE const CLSID CLSID_WbemDecoupledBasicEventProvider;
+TEMPLATE const CLSID CLSID_WbemUnauthenticatedLocator;
 
-EXTERN_C const IID IID_IWbemDecoupledBasicEventProvider;
-EXTERN_C const IID IID_IWbemDecoupledRegistrar;
-EXTERN_C const IID IID_IWbemEventConsumerProvider;
-EXTERN_C const IID IID_IWbemEventProvider;
-EXTERN_C const IID IID_IWbemEventProviderQuerySink;
-EXTERN_C const IID IID_IWbemEventProviderSecurity;
-EXTERN_C const IID IID_IWbemEventSink;
-EXTERN_C const IID IID_IWbemHiPerfProvider;
-EXTERN_C const IID IID_IWbemPropertyProvider;
-EXTERN_C const IID IID_IWbemProviderInitSink;
-EXTERN_C const IID IID_IWbemProviderInit;
-EXTERN_C const IID IID_IWbemProviderIdentity;
-EXTERN_C const IID IID_IWbemUnboundObjectSink;
+TEMPLATE const IID IID_IWbemDecoupledBasicEventProvider;
+TEMPLATE const IID IID_IWbemDecoupledRegistrar;
+TEMPLATE const IID IID_IWbemEventConsumerProvider;
+TEMPLATE const IID IID_IWbemEventProvider;
+TEMPLATE const IID IID_IWbemEventProviderQuerySink;
+TEMPLATE const IID IID_IWbemEventProviderSecurity;
+TEMPLATE const IID IID_IWbemEventSink;
+TEMPLATE const IID IID_IWbemHiPerfProvider;
+TEMPLATE const IID IID_IWbemPropertyProvider;
+TEMPLATE const IID IID_IWbemProviderInitSink;
+TEMPLATE const IID IID_IWbemProviderInit;
+TEMPLATE const IID IID_IWbemProviderIdentity;
+TEMPLATE const IID IID_IWbemUnboundObjectSink;
