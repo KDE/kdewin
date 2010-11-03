814b6c7f6398c5f6fb1127b13e651f7b1e67a850
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2010-11-03 06:02:01 +0000

    fix size_t for win64
    svn path=/trunk/kdesupport/kdewin/; revision=1192433

--- include/msvc/sys/types.h
+++ include/msvc/sys/types.h
@@ -40,7 +40,11 @@ typedef unsigned int nlink_t;
 
 #ifndef _SIZE_T_DEFINED
 #define _SIZE_T_DEFINED
-typedef unsigned int size_t;
+#ifdef  _WIN64
+typedef unsigned __int64 size_t;
+#else
+typedef unsigned int     size_t;
+#endif
 #endif
 
 #ifndef _SSIZE_T_DEFINED
