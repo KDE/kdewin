f26bb8355b8b0a5d74d7482761919a95435f7e83
Ralf Habacker <ralf.habacker@freenet.de>
2013-02-22 14:03:48 +0100

    added BDS license, which makes it able to be included in cmake

--- tools/mt/resourcefile.h
+++ tools/mt/resourcefile.h
@@ -1,6 +1,8 @@
 /* 
    This file is part of the KDE project
-   Copyright (C) 2009 Ralf Habacker  <ralf.habacker@freenet.de>
+
+   Copyright (C) 2009-2013 Ralf Habacker  <ralf.habacker@freenet.de>
+   All Rights Reserved.
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
@@ -17,6 +19,30 @@
    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
    Boston, MA 02110-1301, USA.
 
+   Alternativly this file may be used under the terms of the BSD license. 
+   Redistribution and use in source and binary forms, with or without modification, 
+   are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, 
+       this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright notice, 
+       this list of conditions and the following disclaimer in the documentation 
+       and/or other materials provided with the distribution.
+
+    3. The name of the author may not be used to endorse or promote products derived 
+       from this software without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED BY [LICENSOR] "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+   INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
+   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY 
+   DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+   BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+   OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+   ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+   POSSIBILITY OF SUCH DAMAGE. 
+  
    resource file support for manifest tool 
 */
 #ifndef RESOURCEFILE_H
