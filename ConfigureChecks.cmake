c15686258786b3caff11abe89c8c9fb7d9214231
Ralf Habacker <ralf.habacker@freenet.de>
2017-01-25 23:46:13 +0100

    Fix localtime_r and gmtime_r compile issue in client packages.
    In case kdewin finds the mentioned functions on cmake configure time
    it adds -DKDEWIN_NO_XXX_R to the generated cmake find_package related
    config file. On compiling client packages with kdewin as dependency
    this excludes kdewin's implementation of ...time_r functions.

--- ConfigureChecks.cmake
+++ ConfigureChecks.cmake
@@ -15,6 +15,7 @@ endif (MINGW)
 INCLUDE(CheckTypeSize)
 INCLUDE(CheckFunctionExists)
 INCLUDE(CheckSymbolExists)
+INCLUDE(CheckCSourceCompiles)
 
 SET(CMAKE_EXTRA_INCLUDE_FILES unistd.h)
 CHECK_TYPE_SIZE(size_t KDEWIN_HAVE_SIZE_T)
@@ -23,6 +24,17 @@ CHECK_FUNCTION_EXISTS(usleep        KDEWIN_HAVE_USLEEP)
 CHECK_FUNCTION_EXISTS(sleep         KDEWIN_HAVE_SLEEP)
 CHECK_FUNCTION_EXISTS(fork          KDEWIN_HAVE_FORK)
 CHECK_FUNCTION_EXISTS(truncate      KDEWIN_HAVE_TRUNCATE)
+CHECK_C_SOURCE_COMPILES("#include <time.h>\nint main(int argc, char** argv) { localtime_r(0,0); }" KDEWIN_HAVE_LOCALTIME_R)
+CHECK_C_SOURCE_COMPILES("#include <time.h>\nint main(int argc, char** argv) { gmtime_r(0,0); }" KDEWIN_HAVE_GMTIME_R)
+
+# propagate detection into cmake package config file
+if(KDEWIN_HAVE_LOCALTIME_R)
+    set(KDEWIN_DEFINITIONS "${KDEWIN_DEFINITIONS} -DKDEWIN_NO_LOCALTIME_R")
+endif()
+
+if(KDEWIN_HAVE_GMTIME_R)
+    set(KDEWIN_DEFINITIONS "${KDEWIN_DEFINITIONS} -DKDEWIN_NO_GMTIME_R")
+endif()
 
 configure_file(${CMAKE_CURRENT_SOURCE_DIR}/config.h.cmake ${CMAKE_CURRENT_BINARY_DIR}/kdewin_config.h)
 install (FILES ${CMAKE_CURRENT_BINARY_DIR}/kdewin_config.h DESTINATION include)
